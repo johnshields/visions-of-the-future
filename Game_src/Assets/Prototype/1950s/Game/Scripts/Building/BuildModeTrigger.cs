bb540eae86933d29d0c6c61ee6354451919186fd
John Shields <G00348436@gmit.ie>
2022-02-21 12:14:50 +0000

    Added trigger to build mode.

---
+++ Game_src/Assets/Prototype/1950s/Game/Scripts/Building/BuildModeTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BuildModeTrigger : MonoBehaviour
+{
+    public GameObject[] buildingObjects;
+    private bool _entered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_entered)
+        {
+            print("Build mode on.");
+            buildingObjects[0].SetActive(true);
+            buildingObjects[1].SetActive(true);
+            _entered = true;
+        }
+        else
+        {
+            print("Build mode off.");
+            buildingObjects[0].SetActive(false);
+            buildingObjects[1].SetActive(false);
+            _entered = false;
+        }
+    }
+}
