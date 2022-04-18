28267def975e1a741589a6c94691450727f37eb5
John Shields <G00348436@gmit.ie>
2022-04-18 11:47:25 +0100

    RF: City buidling.

--- Game_src/Assets/Project/Retrofuture/Game/Scripts/DrivingSettings.cs
+++ Game_src/Assets/Project/Retrofuture/Game/Scripts/DrivingSettings.cs
@@ -9,9 +9,12 @@ public class DrivingSettings : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // Music.
         drivingMusic.Play();
+        // Cameras.
         dashCam.SetActive(true);
         mainCam.SetActive(false);
+        //Player & Guide.
         dummy.SetActive(true);
         player.SetActive(false);
         StartCoroutine(Respawn());
@@ -19,14 +22,17 @@ public class DrivingSettings : MonoBehaviour
 
     private IEnumerator Respawn()
     {
+        // Player & Guide.
         player.transform.position = new Vector3(92f, 0.50f, -20f);
         yield return new WaitForSeconds(1f);
         guide.transform.position = new Vector3(98f, 1.5f, -20f);
         guide.SetActive(false);
         dummyGuide.SetActive(true);
         yield return new WaitForSeconds(50f);
+        // Cameras.
         dashCam.SetActive(false);
         mainCam.SetActive(true);
+        // Player & guide.
         dummy.SetActive(false);
         dummyGuide.SetActive(false);
         player.SetActive(true);
