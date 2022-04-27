926cce2a5ed70339bc7c17baef0beaaae3c4e52b
John Shields <johndshields14@gmail.com>
2022-04-27 14:30:58 +0100

    Basic Pause Menu created.

---
+++ Game_src/Assets/Project/MainScripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private static bool _paused;
+    public GameObject menu;
+
+    private void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // if esc pressed pause game else resume.
+        if (!Input.GetKeyDown(KeyCode.Escape)) return;
+        if (_paused) ResumeGame();
+        else PauseGame();
+    }
+
+    private void PauseGame()
+    {
+        // Pause - volume & time.
+        Time.timeScale = 0f;
+        AudioListener.volume = 0f;
+        _paused = true;
+        menu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        // Resume - volume & time.
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+        _paused = false;
+        menu.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("01_MainMenu");
+        _paused = false;
+    }
+}
