ab002c82eb49c7b4b72528987dc9fbed7926561f
Rory Mulhern <rmulhern1998@live.com>
2022-04-27 23:22:00 +0100

    1980s - Interrogation Scene

--- Game_src/Assets/Prototype/1980s/Scripts/DroneScripts/DialogueAnimation.cs
+++ Game_src/Assets/Prototype/1980s/Scripts/DroneScripts/DialogueAnimation.cs
@@ -6,20 +6,15 @@ using TMPro;
 
 public class DialogueAnimation : MonoBehaviour
 {
-    //public static bool isRunning;
-
     public static IEnumerator AnimateDialogue(string ct, string ft, GameObject dt) 
     {
         Debug.Log("Animation Started");
-        //if (isRunning == false) {
             for (int i = 0; i <= ft.Length; i++)
             {
-                //isRunning = true;
                 ct = ft.Substring(0, i);
                 dt.GetComponent<TMP_Text>().text = ct;
                 yield return new WaitForSeconds(0.03f);
             }
-            //isRunning = false;
-        //}
+
     }
 }
--- Game_src/Assets/Prototype/1980s/Scripts/DroneScripts/DroneDialogue.cs
+++ Game_src/Assets/Prototype/1980s/Scripts/DroneScripts/DroneDialogue.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DroneDialogue : MonoBehaviour
 {
@@ -8,11 +10,13 @@ public class DroneDialogue : MonoBehaviour
     public string dialogueFt;
     private const string Ct = "";
 
+    private bool isRunning;
+
     // Start is called before the first frame update
     void Start()
     {
         dialogueUI.SetActive(true);
-        StartCoroutine(DialogueTyper.TypeDialogue(Ct, dialogueFt, dialogueText));
+        StartCoroutine(AnimateDialogue(Ct, dialogueFt, dialogueText));
         StartCoroutine(CloseDialogue());
     }
 
@@ -20,5 +24,21 @@ public class DroneDialogue : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
         dialogueUI.SetActive(false);
+    }
+
+    public IEnumerator AnimateDialogue(string ct, string ft, GameObject dt)
+    {
+        Debug.Log("Animation Started");
+        if (isRunning == false)
+        {
+            isRunning = true;
+            for (int i = 0; i <= ft.Length; i++)
+            {
+                ct = ft.Substring(0, i);
+                dt.GetComponent<TMP_Text>().text = ct;
+                yield return new WaitForSeconds(0.05f);
+            }
+            isRunning = false;
+        }
     }
 }
