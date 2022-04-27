a568680455b45276e6cc22fa360f70fbe9ac23d9
John Shields <johndshields14@gmail.com>
2022-04-27 14:46:17 +0100

    Basic Main Menu created.

---
+++ Game_src/Assets/Project/MainScripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Awake()
+    {
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("NavigationHub");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
